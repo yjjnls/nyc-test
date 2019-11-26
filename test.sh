e88b83a9758e79a40e54a02045dea0233e5b9d98
yuanjunjie <yuanjunjie@baidu.com>
2019-11-26 11:20:54 +0800

    update test script

--- test.sh
+++ test.sh
@@ -3,12 +3,11 @@ git submodule update --init
 cd did-auth-jose
 npm install
 npm test
+npx nyc merge .nyc_output ../.nyc_output/did-auth-jose.json
 cd ../hub-sdk-js
 npm install
 npm test
+npx nyc merge .nyc_output ../.nyc_output/hub-sdk-js.json
 cd ..
 
-npm install nyc
-npx nyc merge did-auth-jose/.nyc_output .nyc_output/did-auth-jose.json
-npx nyc merge hub-sdk-js/.nyc_output .nyc_output/hub-sdk-js.json
 npx nyc report
